beeceb26aeac9e287c96b6c4d942674208268b82
ornwoo96 <dongou705@naver.com>
2023-06-07 18:16:37 +0900

    Initial Commit

---
+++ SwiftUITestProject/SwiftUITestProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUITestProjectApp.swift
+//  SwiftUITestProject
+//
+//  Created by 김동우 on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
