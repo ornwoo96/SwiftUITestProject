f047c8785153eeed8d8b8861f34921c92de91e91
ornwoo96 <dongou705@naver.com>
2023-06-25 12:13:19 +0900

    error: navigationStack으로 딥링크 구현 완료.

--- SwiftUITestProject/PresentationLayer/DeepLink/ToDoList.swift
+++ SwiftUITestProject/PresentationLayer/DeepLink/ToDoList.swift
@@ -17,6 +17,7 @@ func prepareData() -> [TodoItem] {
     
     for i in 0...20 {
         let newTodo = TodoItem(id: UUID(), title: "내 할일 \(i)")
+        print("내 uuid: \(newTodo.id)")
         newList.append(newTodo)
     }
     
@@ -24,43 +25,38 @@ func prepareData() -> [TodoItem] {
 }
 
 struct ToDoList: View {
+    // todoItems로 path를 관리하여 navigation 이동한다.
+    // todoItems에 넣은 값들을 통해 링크를 타고 넘어감
     @State var todoItems = [TodoItem]()
     @State var activeUUID: UUID?
     
-    init() {
-        let data = prepareData()
-        _todoItems = State(initialValue: data)
-        data.forEach {
-            print($0.id)
-        }
-    }
+    let dummyData = prepareData()
     
     var body: some View {
-        NavigationView {
-            List(todoItems) { todoItem in
-                NavigationLink(
-                    destination: Text("\(todoItem.title)"),
-                    tag: todoItem.id,
-                    // activeUUID 값이 변경되면 해당 하는 링크로 이동
-                    selection: $activeUUID
-                ) {
-                    Text("\(todoItem.title)")
-                }
-                
-                
-//                NavigationLink(value: activeUUID,
-//                               label: <#T##() -> View#>)
-                
-//                NavigationLink(
-//                    destination: Text("ㅇㅇㅇ\(todoItem.title)"),
-//                    label: {                    Text("\(todoItem.title)")
-//                })
+        
+        NavigationStack(path: $todoItems) {
+            List(dummyData) { todoItem in
+                NavigationLink("\(todoItem.title)",
+                               value: todoItem)
             }
             .navigationTitle(Text("할 일 목록"))
+            .navigationDestination(for: TodoItem.self) { item in
+                Text("\(item.title)")
+            }
             .onOpenURL(perform: { url in
+                print(url)
                 if case .todoItem(let id) = url.detailPage {
                     print("넘어온 id: \(id)")
                     activeUUID = id
+                    if let first = dummyData.firstIndex(where: {
+                        $0.id == id
+                    }) {
+                        print("찾았다.")
+                        // 여기서 넣은 데이터가 링크타고 넘어가게끔 해드림
+                        todoItems.append(dummyData[first])
+                    } else {
+                        todoItems.removeAll()
+                    }
                 }
             })
         }
